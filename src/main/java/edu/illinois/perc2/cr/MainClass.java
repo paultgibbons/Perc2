2fa94507539acfdd130ae31ae4da51d22c148c0e
Paul Gibbons <paultgibbons@gmail.com>
2016-03-30 09:53:28 -0500

    read in all files

--- src/main/java/edu/illinois/perc2/cr/MainClass.java
+++ src/main/java/edu/illinois/perc2/cr/MainClass.java
@@ -1,6 +1,8 @@
 package edu.illinois.perc2.cr;
 
 import java.io.File;
+import java.io.PrintStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.FilenameFilter;
@@ -15,6 +17,7 @@ import edu.illinois.cs.cogcomp.reader.ace2005.documentReader.AceFileProcessor;
  * @author ptgibbo2
  */
 public class MainClass {
+	private static final String BASE_DIR="data/train/ACE05_English/";
     private static final String TEST_DIR="data/train/ACE05_English/nw";
 	
 	public static List<ACEDocument> readFolder (AceFileProcessor afp, String inputFolderStr) {
@@ -27,13 +30,33 @@ public class MainClass {
 		File[] fileList = inputFolder.listFiles(filter);
 		
 		List<ACEDocument> docs = new ArrayList<ACEDocument>(); 
+		
+		PrintStream out = System.out;
+    	PrintStream err = System.err;
+    	PrintStream devnull = new PrintStream(new OutputStream() {
+            public void write(int b) {
+                //DO NOTHING
+            }
+        });
 			
 		for (int fileID = 0; fileID < fileList.length; ++fileID) {
 
             String annotationFile = fileList[fileID].getAbsolutePath();
-
-            ACEDocument doc = afp.processAceEntry(new File(inputFolderStr), annotationFile);
-            docs.add(doc);
+            
+            try{
+            	
+            	System.setOut(devnull);
+            	System.setErr(devnull);
+            	ACEDocument doc = afp.processAceEntry(new File(inputFolderStr), annotationFile);
+            	System.setOut(out);
+            	System.setErr(err);
+            	docs.add(doc);
+            } catch (Exception e) {
+            	System.setOut(out);
+            	System.setErr(err);
+            	System.err.println("Error on reading flie: "+annotationFile);
+            	System.err.println(e.toString());
+            }
 		}
 		return docs;
 	}
@@ -54,13 +77,24 @@ public class MainClass {
 		return train;
 	}
 	
+	public static List<ACEDocument> readAll(AceFileProcessor afp) {
+		List<ACEDocument> docs = new ArrayList<ACEDocument>();
+		for(String dir : new String[]{"bc","bn","cts","nw","un","wl"}) {
+			String folder = BASE_DIR + dir;
+			docs.addAll(readFolder(afp,folder));
+		}
+		return docs;
+	}
+	
 	
 	public static void main(String[] args) throws Exception {
 		boolean training = true;
 
 		AceFileProcessor afp = new AceFileProcessor(new CcgTextAnnotationBuilder( new IllinoisTokenizer() ));
 		
-		List<ACEDocument> docs = readFolder(afp, TEST_DIR);
+//		List<ACEDocument> docs = readFolder(afp, TEST_DIR);
+		
+		List<ACEDocument> docs = readAll(afp);
 		
 		List<ACEDocument> train = trainDocs(docs);
 		List<ACEDocument> test = testDocs(docs);
